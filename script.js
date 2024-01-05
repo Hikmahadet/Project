aed24dda00fcbb794c95bb64f57d051661a179fc
Hikmahadet <hikmahadetunji14@gmail.com>
2024-01-05 11:30:41 +0100

    adjustments

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const wraper = document.querySelector('.wraper');
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const mitPopup = document.querySelector('.mitSignin-popup');
+registerLink.addEventListener('click', () => {
+    wraper.classList.add('active');
+});
+loginLink.addEventListener("click", () => {
+  wraper.classList.remove("active");
+});
+mitPopup.addEventListener("click", () => {
+  wraper.classList.remove("active-popup");
+});
+
 function myFunction() {
-  var navList = document.getElementById("nav-list");
-  navList.classList.toggle("open");
+    var element = document.getElementById("nav_items");
+    element.classList.toggle('new_list')
 }
