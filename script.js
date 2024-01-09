5f553226c65a0bc9df080190ad7d592813e71bc4
Hikmahadet <hikmahadetunji14@gmail.com>
2024-01-10 00:58:36 +0100

    Touches

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const wraper = document.querySelector('.wraper');
-const loginLink = document.querySelector(".login-link");
-const registerLink = document.querySelector(".register-link");
-const mitPopup = document.querySelector('.mitSignin-popup');
-registerLink.addEventListener('click', () => {
-    wraper.classList.add('active');
-});
-loginLink.addEventListener("click", () => {
-  wraper.classList.remove("active");
-});
-mitPopup.addEventListener("click", () => {
-  wraper.classList.remove("active-popup");
-});
+// const wraper = document.querySelector('.wraper');
+// const loginLink = document.querySelector(".login-link");
+// const registerLink = document.querySelector(".register-link");
+// const mitPopup = document.querySelector('.mitSignin-popup');
+// registerLink.addEventListener('click', () => {
+//     wraper.classList.add('active');
+// });
+// loginLink.addEventListener("click", () => {
+//   wraper.classList.remove("active");
+// });
+// mitPopup.addEventListener("click", () => {
+//   wraper.classList.remove("active-popup");
+// });
 
 function myFunction() {
     var element = document.getElementById("nav_items");
-    element.classList.toggle('new_list')
+    element.classList.toggle('new_list');
+    var element = document.getElementById("right_items");
+    element.classList.toggle("new_list");
 }
+
+ var swiper = new Swiper(".mySwiper", {
+   spaceBetween: 30,
+   centeredSlides: true,
+   autoplay: {
+     delay: 2500,
+     disableOnInteraction: false,
+   },
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+ });
+
+
+ 
